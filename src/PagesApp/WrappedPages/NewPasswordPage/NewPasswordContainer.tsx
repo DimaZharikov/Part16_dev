2139a48fc68742e9c07f618778cbcc7398d62cac
Yuriy <rebusok@yandex.ru>
2021-02-10 15:32:25 +0300

    add confirm

--- src/PagesApp/WrappedPages/NewPasswordPage/NewPasswordContainer.tsx
+++ src/PagesApp/WrappedPages/NewPasswordPage/NewPasswordContainer.tsx
@@ -8,16 +8,20 @@ import {AppRootStateType} from "../../../Redux/Store";
 import {validateInputNewPas} from "../../../Utils/Validation/ValidationPassword";
 import {ChangePassword} from "../../../Redux/NewPassReducer/NewPassReducer";
 import {useHistory} from "react-router-dom";
+import style from './NewPasswordContainer.module.scss'
 
 
 interface Props {
 
 }
 
-const NewPasswordContainer : FC<Props>  = () => {
+const NewPasswordContainer: FC<Props> = () => {
     const [password, setPassword] = useState<string>()
+    const [confirmPassword, setConfirmPassword] = useState<string>()
     const [errorMesPas, setErrorMesPas] = useState<string>('Password Required')
-    const [errorPas, setErrorPas] = useState<boolean>(false)
+    const [errorMesConfirmPas, setErrorMesConfirmPas] = useState<string>('Confirm password Required\'')
+    const [isValidPassword, setIsValidPassword] = useState<boolean>(true)
+    const [isValidConfirmPassword, setIsValidConfirmPassword] = useState<boolean>(false)
     const errorMes = useSelector((state: AppRootStateType) => state.newPas.errorMes)
     const loading = useSelector((state: AppRootStateType) => state.newPas.loading)
 
@@ -29,10 +33,17 @@ const NewPasswordContainer : FC<Props>  = () => {
 
     const token = history.location.pathname.replace(regUrl, "")
 
-
-
-    const changePasswordValue = (value:string) => {
-        validateInputNewPas(setPassword,value, setErrorMesPas, setErrorPas)
+    const validPasswordValue = (value: string) => {
+        validateInputNewPas(setPassword, value, setErrorMesPas, setIsValidPassword)
+    }
+    const validConfirmPasswordValue = (value: string) => {
+        validateInputNewPas(setConfirmPassword, value, setErrorMesConfirmPas, setIsValidConfirmPassword)
+        if (value !== password) {
+            setIsValidConfirmPassword(true)
+            setErrorMesConfirmPas('passwords must match')
+        } else {
+            setIsValidConfirmPassword(false)
+        }
     }
     const changePasswordHandler = () => {
         if (password) {
@@ -41,21 +52,33 @@ const NewPasswordContainer : FC<Props>  = () => {
         setPassword('')
     }
 
-    return(
-        <div>
-        <SuperInputText
-            value={password}
-            onChangeText={changePasswordValue}
-            placeholder={'New Password'}
-            error={errorPas}
-            errorMes={errorMesPas}
-            setError={setErrorPas}
-            type={'password'}
-        />
-        <SuperButton onClick={changePasswordHandler} disabled={loading}>Change password</SuperButton>
-        {loading? <Spinner/> : null}
-        {!!errorMes ? <span>errorMes</span> : null}
-    </div>)
+    return (
+        <div className={style.wrapper_main}>
+            <div className={style.wrapper_items}>
+                <SuperInputText
+                    value={password}
+                    onChangeText={validPasswordValue}
+                    placeholder={'New Password'}
+                    error={isValidPassword}
+                    errorMes={errorMesPas}
+                    setError={setIsValidPassword}
+                    type={'password'}
+                />
+                <SuperInputText
+                    value={confirmPassword}
+                    onChangeText={validConfirmPasswordValue}
+                    placeholder={'Confirm Password'}
+                    error={isValidConfirmPassword}
+                    errorMes={errorMesConfirmPas}
+                    setError={setIsValidConfirmPassword}
+                    type={'password'}
+                />
+                <div><SuperButton onClick={changePasswordHandler} disabled={loading || password !== confirmPassword}>Change
+                    password</SuperButton></div>
+                {loading ? <Spinner/> : null}
+                {!!errorMes ? <span>errorMes</span> : null}
+            </div>
+        </div>)
 }
 
 
